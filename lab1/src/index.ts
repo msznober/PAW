9f5d83f5ff0e77ecd7327116dbef66f5eefa22e8
msznober <61661382+msznober@users.noreply.github.com>
2021-03-07 16:07:44 +0100

    Add files via upload

---
+++ lab1/src/index.ts
@@ -0,0 +1,9 @@
+const input1 = document.querySelector('#first_in');
+const input2 = document.querySelector('#second_in');
+const input3 = document.querySelector('#third_in');
+const input4 = document.querySelector('#fourth_in');
+
+const sum = document.querySelector('#sum');
+const avg = document.querySelector('#avg');
+const min = document.querySelector('#min');
+const max = document.querySelector('#max');
